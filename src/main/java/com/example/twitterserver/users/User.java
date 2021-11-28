8025e8a4255f07e4d807710f3bb8f9f1eb4df7d2
JoseAnnunziato <jose.annunziato@sonos.com>
2021-11-28 10:26:25 -0500

    create users table and insert a couple of users

--- src/main/java/com/example/twitterserver/users/User.java
+++ src/main/java/com/example/twitterserver/users/User.java
@@ -15,4 +15,60 @@ public class User {
     private String lastName;
     private String email;
     private Timestamp joined;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getJoined() {
+        return joined;
+    }
+
+    public void setJoined(Timestamp joined) {
+        this.joined = joined;
+    }
 }
---
+++ src/main/java/com/example/twitterserver/users/users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    user_id int NOT NULL AUTO_INCREMENT,
+    username varchar(255) DEFAULT NULL,
+    password varchar(255) DEFAULT NULL,
+    first_name varchar(255) DEFAULT NULL,
+    last_name varchar(255) DEFAULT NULL,
+    email varchar(255) DEFAULT NULL,
+    joined timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
+);
+
+INSERT INTO users VALUES (null, 'alice', 'alice123', 'Alice', 'Wonderland', 'alice@wonderland.com', CURRENT_TIMESTAMP);
+INSERT INTO users VALUES (null, 'bob', 'bob234', 'Bob', 'Hope', 'bob@hope.com', CURRENT_TIMESTAMP);
+INSERT INTO users VALUES (null, 'charlie', 'brown345', 'Charlie', 'Brown', 'charlie@peanuts.com', CURRENT_TIMESTAMP);
+INSERT INTO users VALUES (null, 'dan', 'dan456', 'Dan', 'Aykroyd', 'dan@007.com', CURRENT_TIMESTAMP);
+INSERT INTO users VALUES (null, 'edward', 'ed567', 'Edward', 'Scizorhands', 'ed@scizors.com', CURRENT_TIMESTAMP);
