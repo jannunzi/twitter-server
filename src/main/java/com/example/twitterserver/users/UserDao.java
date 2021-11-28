55cb882f72f61761ecdb980269e9ac4f2edb720f
JoseAnnunziato <jose.annunziato@sonos.com>
2021-11-28 10:30:39 -0500

    make user dao available as a Web service

--- src/main/java/com/example/twitterserver/users/UserDao.java
+++ src/main/java/com/example/twitterserver/users/UserDao.java
@@ -1,16 +1,23 @@
 package com.example.twitterserver.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class UserDao {
     @Autowired
     UserRepository userRepository;
+    @GetMapping("/api/users")
     public List<User> findAllUsers() {
         return (List<User>) userRepository.findAll();
     }
-    public User findUserById(Integer userId) {
+    @GetMapping("/api/users/{userId}")
+    public User findUserById(
+            @PathVariable("userId") Integer userId) {
         return userRepository.findById(userId).get();
     }
 }
