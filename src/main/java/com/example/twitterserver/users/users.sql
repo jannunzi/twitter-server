ad5e7ac459d3d434baab7de0e0010b61ab1ccf39
JoseAnnunziato <jose.annunziato@sonos.com>
2021-11-28 12:52:04 -0500

    added verified field

--- src/main/java/com/example/twitterserver/users/users.sql
+++ src/main/java/com/example/twitterserver/users/users.sql
@@ -6,11 +6,12 @@ CREATE TABLE users (
     last_name varchar(255) DEFAULT NULL,
     email varchar(255) DEFAULT NULL,
     joined timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    verified bit(1) DEFAULT NULL,
     PRIMARY KEY (user_id)
 );
 
-INSERT INTO users VALUES (null, 'alice', 'alice123', 'Alice', 'Wonderland', 'alice@wonderland.com', CURRENT_TIMESTAMP);
-INSERT INTO users VALUES (null, 'bob', 'bob234', 'Bob', 'Hope', 'bob@hope.com', CURRENT_TIMESTAMP);
-INSERT INTO users VALUES (null, 'charlie', 'brown345', 'Charlie', 'Brown', 'charlie@peanuts.com', CURRENT_TIMESTAMP);
-INSERT INTO users VALUES (null, 'dan', 'dan456', 'Dan', 'Aykroyd', 'dan@007.com', CURRENT_TIMESTAMP);
-INSERT INTO users VALUES (null, 'edward', 'ed567', 'Edward', 'Scizorhands', 'ed@scizors.com', CURRENT_TIMESTAMP);
+INSERT INTO users VALUES (null, 'alice', 'alice123', 'Alice', 'Wonderland', 'alice@wonderland.com', CURRENT_TIMESTAMP, 1);
+INSERT INTO users VALUES (null, 'bob', 'bob234', 'Bob', 'Hope', 'bob@hope.com', CURRENT_TIMESTAMP, 0);
+INSERT INTO users VALUES (null, 'charlie', 'brown345', 'Charlie', 'Brown', 'charlie@peanuts.com', CURRENT_TIMESTAMP, 1);
+INSERT INTO users VALUES (null, 'dan', 'dan456', 'Dan', 'Aykroyd', 'dan@007.com', CURRENT_TIMESTAMP, 0);
+INSERT INTO users VALUES (null, 'edward', 'ed567', 'Edward', 'Scizorhands', 'ed@scizors.com', CURRENT_TIMESTAMP, 1);
