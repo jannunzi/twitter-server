3526fb0aaa79705397c40d00575c87923222e39e
JoseAnnunziato <jose.annunziato@sonos.com>
2021-11-28 10:04:11 -0500

    create User class

---
+++ src/main/java/com/example/twitterserver/users/User.java
@@ -0,0 +1,13 @@
+package com.example.twitterserver.users;
+
+import java.sql.Timestamp;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Timestamp joined;
+}
